20162a09bce3c22db40f2f5742a769fbf46c5f49
Wenlei Dai <resca.wen@gmail.com>
2021-02-21 20:37:45 +0200

    express include build added to fix frontend problem

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
+const express = require('express')
 const jsonServer = require('json-server')
 const api = require('./api')
-// api.getManufacturers()
 
 setInterval(api.getManufacturers, 300000)
-// setInterval(api.getManufacturers, 10)
 
-const server = jsonServer.create()
+// const server = jsonServer.create()
+// const router = jsonServer.router(require('./db.js')())
+// const middlewares = jsonServer.defaults()
+
+// server.use(middlewares)
+// server.use(router)
+// const PORT = process.env.PORT || 3001
+// server.listen(PORT, () => {
+//   console.log('JSON Server is running')
+// })
+
+const server = express()
 const router = jsonServer.router(require('./db.js')())
 const middlewares = jsonServer.defaults()
 
+server.use(express.static('build'))
 server.use(middlewares)
 server.use(router)
 const PORT = process.env.PORT || 3001
